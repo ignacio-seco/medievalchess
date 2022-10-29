bd6374ad693711860fd089a61c1aa2fe8556bd62
Ignácio Seco <itenorioseco@gmail.com>
2022-10-28 21:55:37 -0300

    Just trying to get deeper on the game logic: Foe now, still trying to figure out the setup process of the game, with the army selection and distribution... also changed each player turn so that each can use two characters p/turn. This will make the game more strategic since each player will be having more moves per turn...

--- medieval_chess.js
+++ medieval_chess.js
@@ -48,6 +48,114 @@ console.log(targetSpaceMap)
 
 console.log(cells)
 
+class NewGame{
+    constructor (){
+        this.player1="player1"
+        this.player1Army=[]
+        this.player1ArmyPoints=0
+        this.turnActivations=2
+        this.playerActivationsLast=1
+        this.player2="player2"
+        this.player2Army=[]
+        this.player2ArmyPoints=0
+        this.gameArmyPoints=100
+        this.activePlayer=activePlayer
+    }
+    addPlayer1()
+    {
+       let name //= nameStringqueryselector 
+       if(name!=="")
+       {
+        this.player1=name
+       }
+       else this.player1="Player 1"
+    }
+    addPlayer2()
+    {
+        let name //= nameStringqueryselector 
+        if(name!=="")
+        {
+         this.player2=name
+        }
+        else this.player2="Player 2"
+    }
+    setArmyPoints()
+    {
+        let armyPointsNumber//querySelector;
+        if(armyPointsNumber>100)
+        {
+            if(armyPointsNumber<300)
+            {
+            this.gameArmyPoints=armyPointsNumber
+            } 
+            else this.gameArmyPoints=300 
+        }
+        else this.gameArmyPoints=100
+    }
+    setPlayerOneArmy(type)
+    {
+    let newCharacter = new type (this.player1);
+    if(this.player1ArmyPoints+newCharacter.points<this.gameArmyPoints){
+    this.player1Army.push(newCharacter);
+    let newArmyPoints=0
+    this.player1Army.forEach((element)=>newArmyPoints=newArmyPoints + element.points)
+    this.player1ArmyPoints=newArmyPoints}
+    else 
+    {
+        alert (`${this.player1},this unity cost too much for your army, try a cheaper one or end this fase`);
+    return;
+    }
+
+    }
+    setPlayerTwoArmy(type)
+    {
+    let newCharacter = new type (this.player2);
+    if(this.player2ArmyPoints+newCharacter.points<this.gameArmyPoints){
+    this.player2Army.push(newCharacter);
+    let newArmyPoints=0
+    this.player2Army.forEach((element)=>newArmyPoints=newArmyPoints + element.points)
+    this.player2ArmyPoints=newArmyPoints}
+    else 
+    {
+        alert (`${this.player2},this unity cost too much for your army, try a cheaper one or end this fase`);
+    return;
+    }
+
+    }
+    placePlayerOneArmy(){
+
+    }
+    PlacePlayerTwoArmy(){
+        
+    }
+    setActivePlayer(){
+        this.activePlayer=this.player1
+    }
+
+}
+
+
+
+/*class Player{
+    constructor(playerName)
+    {
+        this.playerName=playerName
+        this.playerArmy=[]
+        this.playerArmyPoints=0
+        this.newCharacter=""
+
+    }
+addArmy(type){this.newCharacter = new type (this.playerName);
+    playerArmy.push(this.newCharacter);
+    let newArmyPoints=0
+    playerArmy.forEach((element)=>newArmyPoints=newArmyPoints + element.points)
+    this.playerArmyPoints=armyPoints
+
+}
+}*/
+
+
+
 
 
 
